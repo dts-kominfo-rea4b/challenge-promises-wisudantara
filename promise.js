04db52d1805139d0efbb3f5aab7ac0d7601d35bd
mp_windari <pheewind@gmail.com>
2022-09-19 00:10:33 +0700

    Modify promise.js

--- promise.js
+++ promise.js
@@ -1,7 +1,23 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) =>{
+  counter = 0;
+  const IIX  = await promiseTheaterIXX();
+  const VGC = await promiseTheaterVGC();
+      
+  for (hasil of IIX){
+      if (hasil['hasil'] == emosi){
+          counter++
+      }
+  }
+  for (hasil of VGC){
+      if (hasil['hasil'] == emosi){
+          counter++
+      }
+  }
+  return counter;
+};
 
 module.exports = {
   promiseOutput,
